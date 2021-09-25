24e9bb4afcec7d41f101446a1091b751e8a94a6e
yehang218 <lovesh618@naver.com>
2021-09-26 03:58:53 +0900

    Feat : 상점 버튼 기능 변경
    로비에서 도장, 책상, 인주 눌렀을 때 상점 화면으로 가는 것이 아니라  로비에서 보여지게 함 (구매 기능은 구현X)

--- TeamODD.ver0.0.3/Assets/HubButton.cs
+++ TeamODD.ver0.0.3/Assets/HubButton.cs
@@ -19,23 +19,19 @@ public class HubButton : MonoBehaviour
     public static Text 자본확인용;
     public static Text 점수확인용;
 
-    public static int ListNum=0, Maxs;
-    public static string[] ItemGroup=new string[] { "기본 스킨","커스텀 1", "커스텀 2", "커스텀 3", "커스텀 4", "커스텀 5", "\0"}; //추후 다른 파일에서 불러오도록 설정
-    int[] StampGroup = new int[] { 0, 1, 2, 3, 4, 5};
-    int[] ShapeGroup = new int[] { 0, 1, 2, 3, 4, 5 };
-   
+    public static int ListNum = 0, Maxs;
+    
+    // 가운데 글씨 아이템 이름 설정
+    public static string[] ItemGroup_S = new string[] { "기본 스킨", "벚꽃", "하트", "주먹", "출시 예정", "출시 예정", "\0" }; //추후 다른 파일에서 불러오도록 설정
+    public static string[] ItemGroup_T = new string[] { "기본 스킨", "꽃", "하트", "스크래치", "출시예정", "출시예정" };
+    public static string[] ItemGroup_I = new string[] { "기본 스킨", "꽃무늬", "하트", "벽돌", "출시예정", "출시예정" };
 
-    void Start()
+    int[] StampGroup = new int[] { 0, 1, 2, 3, 4, 5 };
+
+
+    private void Awake()
     {
-        Canvas = GameObject.Find("GroupGameButton");
-        MatchLoading = GameObject.Find("Matching");
-
-        choseStampSkin();
-        choseStampInkSkin();
-
         SaveData.DoLoadData = true;
-
-        STGroup.GetComponent<Text>().text = ItemGroup[ListNum];
 
         이름확인용 = GameObject.Find("TestName").GetComponent<Text>();
         레벨확인용 = GameObject.Find("TestDuty").GetComponent<Text>();
@@ -45,102 +41,196 @@ public class HubButton : MonoBehaviour
         자본확인용.GetComponent<Text>().text = SaveData.Money.ToString();
         점수확인용.GetComponent<Text>().text = SaveData.Points.ToString();
 
+        //여기 왜 오류나징
+        if (SceneManager.GetActiveScene().name == "SampleScene")
+        {
+            STGroup.GetComponent<Text>().text = ItemGroup_S[ListNum];
+            StempsImage.GetComponent<Image>().sprite = ItemBundss.Stamp_N;
+            ShapeImage.GetComponent<Image>().sprite = ItemBundss.Stamp_N_Ink;
+        }
+
+        else if (SceneManager.GetActiveScene().name == "SampleScene_T")
+        {
+            STGroup.GetComponent<Text>().text = ItemGroup_T[ListNum];
+            StempsImage.GetComponent<Image>().sprite = ItemBundss.Table_N;
+            //ShapeImage.GetComponent<Image>().sprite = Testsss;
+        }
+
+        else if (SceneManager.GetActiveScene().name == "SampleScene_I")
+        {
+            STGroup.GetComponent<Text>().text = ItemGroup_I[ListNum];
+            StempsImage.GetComponent<Image>().sprite = ItemBundss.Ink_N;
+            ShapeImage.GetComponent<Image>().sprite = Testsss;
+        }
+
         UnityEngine.Debug.Log(SaveData.Money);
+    }
+
+    void Start()
+    {
+        Canvas = GameObject.Find("GroupGameButton");
+        MatchLoading = GameObject.Find("Matching");
+        ListNum = 0;
+
+        choseStampSkin();
+
+        
+
     }
     public void FAR()
     {
         ListNum++;
-        if (ListNum == Maxs+1)
+        if (ListNum == Maxs + 1)
             ListNum = 0;
-        STGroup.GetComponent<Text>().text = ItemGroup[ListNum];
+
+        // 버튼 가운데 아이템 이름 설정
+        if (SceneManager.GetActiveScene().name == "SampleScene")
+        {
+            STGroup.GetComponent<Text>().text = ItemGroup_S[ListNum];
+        }
+
+        else if(SceneManager.GetActiveScene().name == "SampleScene_T")
+        {
+            STGroup.GetComponent<Text>().text = ItemGroup_T[ListNum];
+        }
+
+        else if(SceneManager.GetActiveScene().name == "SampleScene_I")
+        {
+            STGroup.GetComponent<Text>().text = ItemGroup_I[ListNum];
+        }
+        
         choseStampSkin();
-        choseStampInkSkin();
         //if (StampGroup[ListNum] == 3) { StempsImage.GetComponent<Image>().sprite = Testsss; } //나중에 임시저장 형식으로 만들어서 불러오기 가능하게 & 해당 번호에 따른 스프라이트 지정 함수를 만들기
 
         //UnityEngine.Debug.Log("Front Arrow");
     }
 
-    public void choseStampInkSkin()
-    {
-        if (StampGroup[ListNum] == 0)
-        {
-           // Debug.Log("기본스킨 + Stamp_N_Ink");
-            ShapeImage.GetComponent<Image>().sprite = ItemBundss.Stamp_N_Ink;
-        }
-        else if (StampGroup[ListNum] == 1)
-        {
-            if (SaveData.Stamp_Get[0] == true)
-            {
-                //Debug.Log("커스텀1 + Stamp_1");
-                ShapeImage.GetComponent<Image>().sprite = ItemBundss.Stamp_1;
-            }
-            else ShapeImage.GetComponent<Image>().sprite = Testsss;
-        }
-        else if (StampGroup[ListNum] == 2)
-        {
-            if (SaveData.Stamp_Get[1] == true)
-            {
-                //Debug.Log("커스텀2 + Stamp_2");
-                ShapeImage.GetComponent<Image>().sprite = ItemBundss.Stamp_2;
-            }
-            else ShapeImage.GetComponent<Image>().sprite = Testsss;
-        }
-        else if (StampGroup[ListNum] == 3)
-        {
-            if (SaveData.Stamp_Get[2] == true)
-            {
-                //Debug.Log("커스텀3 + Stamp_3");
-                ShapeImage.GetComponent<Image>().sprite = ItemBundss.Stamp_3;
-            }
-            else ShapeImage.GetComponent<Image>().sprite = Testsss;
-        }
-        else ShapeImage.GetComponent<Image>().sprite = Testsss;
-    }
-
     public void choseStampSkin()
     {
-        if (StampGroup[ListNum] == 0)
+        if (SceneManager.GetActiveScene().name == "SampleScene")
         {
-            //Debug.Log("기본스킨 + Stamp_N");
-            StempsImage.GetComponent<Image>().sprite = ItemBundss.Stamp_N;
-        }
-        else if (StampGroup[ListNum] == 1)
-        {
-            if (SaveData.Stamp_Get[0] == true)
+            if (StampGroup[ListNum] == 0)
+            {
+                // Debug.Log("기본스킨 + Stamp_N_Ink");
+                StempsImage.GetComponent<Image>().sprite = ItemBundss.Stamp_N;
+                ShapeImage.GetComponent<Image>().sprite = ItemBundss.Stamp_N_Ink;
+            }
+            else if (StampGroup[ListNum] == 1)
             {
                 //Debug.Log("커스텀1 + Stamp_1");
                 StempsImage.GetComponent<Image>().sprite = ItemBundss.Stamp_1;
+                ShapeImage.GetComponent<Image>().sprite = ItemBundss.Stamp_1_Ink;
+
             }
-            else StempsImage.GetComponent<Image>().sprite = Testsss;
-        }
-        else if (StampGroup[ListNum] == 2) {
-            if (SaveData.Stamp_Get[1] == true)
+            else if (StampGroup[ListNum] == 2)
             {
-                //Debug.Log("커스텀2 + Stamp_2");
                 StempsImage.GetComponent<Image>().sprite = ItemBundss.Stamp_2;
+                ShapeImage.GetComponent<Image>().sprite = ItemBundss.Stamp_2_Ink;
+
             }
-            else StempsImage.GetComponent<Image>().sprite = Testsss;
-        }
-        else if (StampGroup[ListNum] == 3)
-        {
-            if (SaveData.Stamp_Get[2] == true)
+            else if (StampGroup[ListNum] == 3)
             {
-                //Debug.Log("커스텀3 + Stamp_3");
                 StempsImage.GetComponent<Image>().sprite = ItemBundss.Stamp_3;
+                ShapeImage.GetComponent<Image>().sprite = ItemBundss.Stamp_3_Ink;
+
             }
-            else StempsImage.GetComponent<Image>().sprite = Testsss;
+            else
+            {
+                StempsImage.GetComponent<Image>().sprite = Testsss;
+                ShapeImage.GetComponent<Image>().sprite = Testsss;
+            }
         }
-        else StempsImage.GetComponent<Image>().sprite = Testsss;
+
+        else if (SceneManager.GetActiveScene().name == "SampleScene_T")
+        {
+            if (StampGroup[ListNum] == 0)
+            {
+                StempsImage.GetComponent<Image>().sprite = ItemBundss.Table_N;
+                //ShapeImage.GetComponent<Image>().sprite = Testsss;
+            }
+            else if (StampGroup[ListNum] == 1)
+            {
+                //Debug.Log("커스텀1 + Stamp_1");
+                StempsImage.GetComponent<Image>().sprite = ItemBundss.Table_1;
+                //ShapeImage.GetComponent<Image>().sprite = Testsss;
+
+            }
+            else if (StampGroup[ListNum] == 2)
+            {
+                StempsImage.GetComponent<Image>().sprite = ItemBundss.Table_2;
+                //ShapeImage.GetComponent<Image>().sprite = Testsss;
+
+            }
+            else if (StampGroup[ListNum] == 3)
+            {
+                StempsImage.GetComponent<Image>().sprite = ItemBundss.Table_3;
+                //ShapeImage.GetComponent<Image>().sprite = Testsss;
+
+            }
+            else
+            {
+                StempsImage.GetComponent<Image>().sprite = Testsss;
+                //ShapeImage.GetComponent<Image>().sprite = Testsss;
+            }
+        }
+
+        else if (SceneManager.GetActiveScene().name == "SampleScene_I")
+        {
+            if (StampGroup[ListNum] == 0)
+            {
+                StempsImage.GetComponent<Image>().sprite = ItemBundss.Ink_N;
+                ShapeImage.GetComponent<Image>().sprite = ItemBundss.Stamp_N;
+            }
+            else if (StampGroup[ListNum] == 1)
+            {
+                //Debug.Log("커스텀1 + Stamp_1");
+                StempsImage.GetComponent<Image>().sprite = ItemBundss.Ink_1;
+                ShapeImage.GetComponent<Image>().sprite = Testsss;
+
+            }
+            else if (StampGroup[ListNum] == 2)
+            {
+                StempsImage.GetComponent<Image>().sprite = ItemBundss.Ink_2;
+                ShapeImage.GetComponent<Image>().sprite = Testsss;
+
+            }
+            else if (StampGroup[ListNum] == 3)
+            {
+                StempsImage.GetComponent<Image>().sprite = ItemBundss.Ink_3;
+                ShapeImage.GetComponent<Image>().sprite = Testsss;
+
+            }
+            else
+            {
+                StempsImage.GetComponent<Image>().sprite = Testsss;
+                ShapeImage.GetComponent<Image>().sprite = Testsss;
+            }
+        }
     }
+
+
+ 
 
     public void BAR()
     {
         ListNum--;
         if (ListNum == -1)
             ListNum = Maxs;
-        STGroup.GetComponent<Text>().text = ItemGroup[ListNum];
+        if (SceneManager.GetActiveScene().name == "SampleScene")
+        {
+            STGroup.GetComponent<Text>().text = ItemGroup_S[ListNum];
+        }
+
+        else if (SceneManager.GetActiveScene().name == "SampleScene_T")
+        {
+            STGroup.GetComponent<Text>().text = ItemGroup_T[ListNum];
+        }
+
+        else if (SceneManager.GetActiveScene().name == "SampleScene_I")
+        {
+            STGroup.GetComponent<Text>().text = ItemGroup_I[ListNum];
+        }
         choseStampSkin();
-        choseStampInkSkin();
         //UnityEngine.Debug.Log("Back Arrow");
     }
 
@@ -159,17 +249,17 @@ public class HubButton : MonoBehaviour
     public void ShopStampSkins()
     {
         //UnityEngine.Debug.Log("Open Stamp");
-        SceneManager.LoadScene("Store");
+        SceneManager.LoadScene("SampleScene");
     }
     public void ShopTableSkins()
     {
         //UnityEngine.Debug.Log("Open Table");
-        SceneManager.LoadScene("Store_T");
+        SceneManager.LoadScene("SampleScene_T");
     }
     public void ShopInkSkins()
     {
         //UnityEngine.Debug.Log("Open Ink");
-        SceneManager.LoadScene("Store_I");
+        SceneManager.LoadScene("SampleScene_I");
     }
     public void Options()
     {
@@ -184,9 +274,30 @@ public class HubButton : MonoBehaviour
 
     void Update()
     {
+        
+        if (SceneManager.GetActiveScene().name == "SampleScene")
+        {
+            STGroup.GetComponent<Text>().text = ItemGroup_S[ListNum];
+            StempsImage.GetComponent<Image>().sprite = Selecting_Stuff_Lobby("Stamp", StampGroup[ListNum]);
+            ShapeImage.GetComponent<Image>().sprite = Selecting_Stuff_Lobby("Stamp_Ink", StampGroup[ListNum]);
+        }
+
+        else if (SceneManager.GetActiveScene().name == "SampleScene_T")
+        {
+            STGroup.GetComponent<Text>().text = ItemGroup_T[ListNum];
+            StempsImage.GetComponent<Image>().sprite = Selecting_Stuff_Lobby("Table", StampGroup[ListNum]);
+            //ShapeImage.GetComponent<Image>().sprite = Testsss;
+        }
+
+        else if (SceneManager.GetActiveScene().name == "SampleScene_I")
+        {
+            STGroup.GetComponent<Text>().text = ItemGroup_I[ListNum];
+            StempsImage.GetComponent<Image>().sprite = Selecting_Stuff_Lobby("Ink", StampGroup[ListNum]);
+            ShapeImage.GetComponent<Image>().sprite = Selecting_Stuff_Lobby("Ink_Color", StampGroup[ListNum]);
+        }
         for (int x=0; ; x++)
         {
-            if (ItemGroup[x].Equals("\0"))
+            if (ItemGroup_S[x].Equals("\0")|| ItemGroup_T[x].Equals("\0")|| ItemGroup_I[x].Equals("\0"))
             {
                 Maxs = x - 1;
                 break;
@@ -196,5 +307,55 @@ public class HubButton : MonoBehaviour
         {
             Application.Quit();
         }
+    }
+
+    public Sprite Selecting_Stuff_Lobby(string Type, int Number)
+    {
+        if(Type == "Stamp")
+        {
+            if (Number == 0) return ItemBundss.Stamp_N;
+            else if (Number == 1) return ItemBundss.Stamp_1;
+            else if (Number == 2) return ItemBundss.Stamp_2;
+            else if (Number == 3) return ItemBundss.Stamp_3;
+            else return Testsss;
+        }
+
+        else if (Type == "Stamp_Ink")
+        {
+            if (Number == 0) return ItemBundss.Stamp_N_Ink;
+            else if (Number == 1) return ItemBundss.Stamp_1_Ink;
+            else if (Number == 2) return ItemBundss.Stamp_2_Ink;
+            else if (Number == 3) return ItemBundss.Stamp_3_Ink;
+            else return Testsss;
+        }
+
+        else if (Type == "Table")
+        {
+            if (Number == 0) { return ItemBundss.Table_N; }
+            else if (Number == 1) { return ItemBundss.Table_1; }
+            else if (Number == 2) { return ItemBundss.Table_2; }
+            else if (Number == 3) { return ItemBundss.Table_3; }
+            else return Testsss;
+        }
+
+        else if(Type == "Ink")
+        {
+            if (Number == 0) { return ItemBundss.Ink_N; }
+            else if (Number == 1) { return ItemBundss.Ink_1; }
+            else if (Number == 2) { return ItemBundss.Ink_2; }
+            else if (Number == 3) { return ItemBundss.Ink_3; }
+            else return Testsss;
+        }
+
+        else if (Type == "Ink_Color")
+        {
+            if (Number == 0) { return ItemBundss.Stamp_N_Ink; }
+            else if (Number == 1) { return ItemBundss.Stamp_N_Ink; }
+            else if (Number == 2) { return ItemBundss.Stamp_N_Ink; }
+            else if (Number == 3) { return ItemBundss.Stamp_N_Ink; }
+            else return Testsss;
+        }
+
+        return ItemBundss.ERROR;
     }
 }
