65afbbe9c64431dbce44116f7e070793ad79cebc
Hyun_W <69100145+yehang218@users.noreply.github.com>
2022-08-07 18:03:14 +0900

    리셋버튼 수정

--- TeamODD.ver0.0.3/Assets/Scripts/ButtonScript.cs
+++ TeamODD.ver0.0.3/Assets/Scripts/ButtonScript.cs
@@ -35,7 +35,7 @@ public class ButtonScript : MonoBehaviour
 
     public void OnResetClick()
     {
-        if ((!is_Stop) && !GeneratorControllerScript.success)
+        if ((!is_Stop) && BoardControllerScript.otherTouchCount == 0)
         {
             GameObject.Find("GameController").GetComponent<BoardControllerScript>().StampBoardReset();
             GameObject.Find("SealingWaxDummyObj").GetComponent<SealingWaxScript2>().WaxOnOff();
